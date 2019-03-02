b32a5cfdf1d1c650dcc0d4546af1d458031fe42b
jhnferraris <johnmichaelferraris@gmail.com>
2019-03-02 19:55:58 +0800

    Added GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: 'UA-127060662-1'
       }
     },
     `gatsby-plugin-feed`,
